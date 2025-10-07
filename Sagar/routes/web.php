9bd4b5f3143802312880cf10c8d97933e7150a07
Subhanshu Pardeshi <subhanshup.15@gmail.com>
2025-10-07 18:35:16 +0530

    website process

---
+++ Sagar/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\frontend\AboutController;
+use App\Http\Controllers\frontend\ContactController;
+use App\Http\Controllers\frontend\HomeController;
+use App\Http\Controllers\frontend\PortfolioController;
+use App\Http\Controllers\frontend\ServicesController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', [HomeController::class, 'index']);
+
+
+Route::get('/about', [AboutController::class, 'index']);
+
+Route::get('/contact', [ContactController::class, 'index']);
+
+Route::get('/port', [PortfolioController::class, 'index']);
+
+Route::get('/serv', [ServicesController::class, 'index']);
+
